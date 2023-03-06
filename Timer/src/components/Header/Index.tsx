ffed46ea019f3f4d191e287489009cdcc7c965b2
Fellipe Pinho <fellipe_mansur@yahoo.com.br>
2023-03-06 18:56:48 -0300

    hooks

--- Timer/src/components/Header/Index.tsx
+++ Timer/src/components/Header/Index.tsx
@@ -1,8 +1,8 @@
 import { HeaderContainer } from './styles'
 
 import logoIgnite from '../../assets/logo-ignite.svg'
-import { Timer } from 'phosphor-react'
-import Scroll from 'phosphor-react/dist/icons/Scroll'
+import { Scroll, Timer } from 'phosphor-react'
+
 import { NavLink } from 'react-router-dom'
 
 export function Header() {
